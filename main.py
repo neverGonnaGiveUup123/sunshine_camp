b5d0d865f0bdab863aa9f52ad0c35d4359ec8e77
Siddharth <st22041@ormiston.school.nz>
2023-03-23 21:29:24 +1300

    Add delete option, removed info update for fixes

--- main.py
+++ main.py
@@ -43,26 +43,36 @@ class sunshine_camp_gui(ttkb.Window):
         def update_button_func():
             data[name_entry.get()] = [location_entry.get(), number_of_campers_entry.get(), weather_entry.get()]
             for i in enumerate(data.keys()):
-                ttkb.Label(self, text=i[1]).grid(row=i[0] + 7,column=0, pady=5)
-                ttkb.Label(self, text=data[i[1]][0]).grid(row=i[0] + 7, column=1, pady=5)
-                ttkb.Label(self, text=data[i[1]][1]).grid(row=i[0] + 7, column=2, pady=5)
-                ttkb.Label(self, text=data[i[1]][2]).grid(row=i[0] + 7, column=3, pady=5)
+                pass
+        
+        def delete_button_func():
+            pass
 
 
         update_button = ttkb.Button(self, text="Update information", bootstyle=INFO, command=update_button_func)
         update_button.grid(row=5,column=1,columnspan=2,padx=20,pady=20)
 
+        delete_label = ttkb.Label(self,text="Enter leader name to delete group: ")
+        delete_label.grid(row=6,column=1,padx=10,pady=10)
+        delete_entry = ttkb.Entry(self)
+        delete_entry.grid(row=6,column=2,padx=10,pady=10)
+        delete_button = ttkb.Button(self, text="Delete group", bootstyle=DANGER, command=delete_button_func)
+        delete_button.grid(row=7, column=1, columnspan=2, padx=10,pady=10)
+
+        error_label = ttkb.Label(self, text=" ")
+        error_label.grid(row=8,column=1,columnspan=2,padx=10,pady=10)
+
         num_of_campers_output = ttkb.Label(self, text="Number of campers: ")
-        num_of_campers_output.grid(row=6,column=2,padx=10,pady=5)
+        num_of_campers_output.grid(row=9,column=2,padx=10,pady=5)
 
         location_output = ttkb.Label(self, text="Location: ")
-        location_output.grid(row=6,column=1,padx=10,pady=5)
+        location_output.grid(row=9,column=1,padx=10,pady=5)
 
         weather_output = ttkb.Label(self, text="Weather: ")
-        weather_output.grid(row=6,column=3,padx=10,pady=5)
+        weather_output.grid(row=9,column=3,padx=10,pady=5)
 
         names_label = ttkb.Label(self, text="Names: ")
-        names_label.grid(row=6, column=0, padx=10,pady=5)
+        names_label.grid(row=9, column=0, padx=10,pady=5)
 
 app = sunshine_camp_gui()
 app.mainloop()
